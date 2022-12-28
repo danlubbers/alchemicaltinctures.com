9ff42022a9abbea448c8ae678ef9297fa48bbae1
Dan Lubbers <lubbers.dan@gmail.com>
2022-12-28 16:01:23 -0500

    chore: added meta description & favicon

--- app/root.tsx
+++ app/root.tsx
@@ -16,6 +16,8 @@ export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Alchemical Tinctures",
+  description:
+    'Locally foraged fungi / mushrooms / flowers around the Ohio Valley region in the USA! All of our tinctures are created from fruiting bodies / aerial flower parts using the double extraction process that involves a 6 week alcohol maceration in 100 proof "gluten free" vodka followed by an 8 hour hot water decoction using spring water for optimal extraction of both alcohol and water soluble constituents. ',
   viewport: "width=device-width,initial-scale=1",
 });
 
